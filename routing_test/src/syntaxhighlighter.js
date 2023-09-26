aa53c37b5fe224ff972a226159433aafc68c67fd
bigboyfredo <andreas.hurlen.kristiansen@gmail.com>
2023-09-26 20:23:36 +0200

    syntax highlighter now better

--- routing_test/src/syntaxhighlighter.js
+++ routing_test/src/syntaxhighlighter.js
@@ -1,8 +1,7 @@
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import coldark_dark from 'react-syntax-highlighter/dist/esm/styles/prism/coldark-dark';
 
-const SYNTAX_HIGHLIGHTING_STYLE = atomOneDark;
-
+const SYNTAX_HIGHLIGHTING_STYLE = coldark_dark
 const highlightLines = (lines) => {
     return (n) => {
         const props = {
@@ -21,7 +20,7 @@ export const DefaultHighlighter = SyntaxHighlighter;
 
 export const Highlighter = (props) => {
     return (
-        <SyntaxHighlighter language={props.language} customStyle={{ padding: '' }} lineNumberStyle={{'min-width': ''}} className={props.className || 'big_code'} codeTagProps={{ className: props.codeTagClassName || 'big_code_wrapper' }} style={SYNTAX_HIGHLIGHTING_STYLE} wrapLines={true} PreTag={'code'} CodeTag={'div'} useInlineStyles={true} showLineNumbers={true} showInlineLineNumbers={true} lineProps={highlightLines(props.linesToHighlight)}>
+        <SyntaxHighlighter language={props.language} customStyle={{ padding: '' }} lineNumberStyle={{ 'min-width': '' }} className={props.className || 'big_code'} codeTagProps={{ className: props.codeTagClassName || 'big_code_wrapper' }} style={SYNTAX_HIGHLIGHTING_STYLE} wrapLines={true} PreTag={'code'} CodeTag={'div'} useInlineStyles={true} showLineNumbers={true} showInlineLineNumbers={true} lineProps={highlightLines(props.linesToHighlight)}>
             {props.children}
         </SyntaxHighlighter>
     );
