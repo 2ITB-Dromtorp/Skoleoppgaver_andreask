1528c9a491ff60538b8242f74c795a636c156c5b
bigboyfredo <andreas.hurlen.kristiansen@gmail.com>
2023-11-10 23:37:48 +0100

    rah

--- authentication/src/pages/index/pages/index.js
+++ authentication/src/pages/index/pages/index.js
@@ -46,25 +46,25 @@ const Index = ({ ...props }) => {
                 <LargeCard name='Popular among companies' desc='Used by all the major companies in NobodyVille. And when i say all the major i mean 100% of them.'>
                     <img className='home_card_image' src='https://previews.123rf.com/images/ab7272/ab72722004/ab7272200400182/143862490-a-big-black-african-pig-with-a-dirty-face-looks-straight-at-the-camera-through-the-bars-the-concept.jpg' />
                 </LargeCard>
-                <LargeCard name='Specialized for everybody' desc={`Specialized to fit every persons needs, whether you're a working privately or a businessman.`}>
+                <LargeCard name='Specialized for anybody' desc={`Specialized to fit any persons needs, whether you're working privately or a businessman.`}>
                     <img className='home_card_image' src='https://previews.123rf.com/images/ab7272/ab72722004/ab7272200400182/143862490-a-big-black-african-pig-with-a-dirty-face-looks-straight-at-the-camera-through-the-bars-the-concept.jpg' />
                 </LargeCard>
-                <LargeCard name='Specialized for everybody' desc={`Specialized to fit every persons needs, whether you're a working privately or a businessman.`}>
+                <LargeCard name='Simplifies the daily life' desc={`Made to be easy to use and uncomplicated while still performing complicated tasks effortlessly.`}>
                     <img className='home_card_image' src='https://previews.123rf.com/images/ab7272/ab72722004/ab7272200400182/143862490-a-big-black-african-pig-with-a-dirty-face-looks-straight-at-the-camera-through-the-bars-the-concept.jpg' />
                 </LargeCard>
-                <SmallCard name='Optimized' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
+                <SmallCard name='Optimized' desc={`Optimized to run optimal on 2012 androids. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
 
                 </SmallCard>
-                <SmallCard name='Developed by the best' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
+                <SmallCard name='Developed by the best' desc={`Developed by me obviously. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
 
                 </SmallCard>
-                <SmallCard name='Maintained' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
+                <SmallCard name='Maintained' desc={`Maintained by me obviously. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
 
                 </SmallCard>
-                <SmallCard name='Simply the best' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
+                <SmallCard name='Simply the best' desc={`Its the best cuz its made by me. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
 
                 </SmallCard>
-                <SmallCard name='Word sucks use this instead' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
+                <SmallCard name='Word sucks use this instead' desc={`Word is overrated. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
 
                 </SmallCard>
                 <SmallCard name='The future' desc={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. `}>
